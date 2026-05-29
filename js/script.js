905d167620f36224223e0f10b239b84f747e4807
262002-stack <262002@virtualwindow.co.za>
2026-05-29 12:15:29 +0200

    whaahtt

--- js/script.js
+++ js/script.js
@@ -5,52 +5,66 @@
 // document.getElementById("sneeky").addEventListener("click",test);
 
 // Get the modal
-var modal = document.getElementById("myModal");
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() 
-{
-  modal.style.display = "block";
-}
+// btn.onclick = function() 
+// {
+//   modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() 
-{
-  modal.style.display = "none";
-}
+// span.onclick = function() 
+// {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 
 ///////form stuff :P
 
-let bigForm = document.getElementById("bigForm")
-let username = document.getElementById("name")
-let welcomeMessage = document.getElementById("welcomeMessage")
+let bigForm = document.forms['bigForm'];
+bigForm.addEventListener("submit",welcomeMessage);
 
-//message
+function welcomeMessage(event){
+  event.preventDefault();
+  let username = document.getElementById("name").value;
+  let output = `
+    Thank you ${username} we will get back to you as soon as possible
+  `;
+  document.querySelector(".outputContainer").innerHTML = output;
+  let thankYouModal = new bootstrap.Modal(document.getElementById('thankYouModal'));
+  thankYouModal.show();
+  
 
-let currentUsername = username.value.trim();
-displayWelcomeMessage(currentUsername);
-
-//displays message 
-let displayWelcomeMessage = (currentUsername) =>
-{
-  //crate output send with innerhtml to right place 
-  let output = `Welcome, ${currentUsername}!`;
-  welcomeMessage.innerHTML = output;
 
 };
 
-bigForm.addEventListener("submit");
+
+//message
+
+// let currentUsername = username.value.trim();
+// displayWelcomeMessage(currentUsername);
+
+// //displays message 
+// let displayWelcomeMessage = (currentUsername) =>
+// {
+//   //crate output send with innerhtml to right place 
+//   let output = `Welcome, ${currentUsername}!`;
+//   welcomeMessage.innerHTML = output;
+
+// };
+
+// bigForm.addEventListener("submit");
