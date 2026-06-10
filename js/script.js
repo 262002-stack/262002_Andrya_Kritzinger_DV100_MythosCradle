56d28c7ca38d25ab8e4ea90f9738778812fd48bc
262002-stack <262002@virtualwindow.co.za>
2026-06-10 10:25:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,42 +47,6 @@ let products = [
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 
-
-// let test = () => {
-//     document.getElementById("sneeky").textContent = "Added to cart!";
-// };
-
-// document.getElementById("sneeky").addEventListener("click",test);
-
-// Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-// btn.onclick = function() 
-// {
-//   modal.style.display = "block";
-// }
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() 
-// {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-
 ///////form stuff :P
 
 let bigForm = document.forms['bigForm'];
@@ -103,22 +67,7 @@ function welcomeMessage(event){
 };
 
 
-//message
-
-// let currentUsername = username.value.trim();
-// displayWelcomeMessage(currentUsername);
-
-// //displays message 
-// let displayWelcomeMessage = (currentUsername) =>
-// {
-//   //crate output send with innerhtml to right place 
-//   let output = `Welcome, ${currentUsername}!`;
-//   welcomeMessage.innerHTML = output;
-
-// };
-
-
-
+///cart
 
 function addProductToCart(productId) {
 
